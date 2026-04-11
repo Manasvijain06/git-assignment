3020b39192aa424e0d058fbd8be294526aebaf68
manasvijain06 <manasvijain61104@gmail.com>
2026-04-11 13:39:57 +0530

    feat: Add custom UserNotFoundException for handling user not found scenarios

---
+++ manasvijain_java_training/session2/user_management/src/main/java/com/nucleusteq/user_management/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nucleusteq.user_management.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
