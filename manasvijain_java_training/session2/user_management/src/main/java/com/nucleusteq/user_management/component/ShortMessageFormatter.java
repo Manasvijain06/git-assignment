f326781ffca8ec9c3dfbb5bb3b5050659dd1f827
manasvijain06 <manasvijain61104@gmail.com>
2026-04-11 13:29:14 +0530

    feat: Implement ShortMessageFormatter components

---
+++ manasvijain_java_training/session2/user_management/src/main/java/com/nucleusteq/user_management/component/ShortMessageFormatter.java
@@ -0,0 +1,11 @@
+package com.nucleusteq.user_management.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShortMessageFormatter {
+
+    public String formatMessage() {
+        return "Hello!";
+    }
+}
