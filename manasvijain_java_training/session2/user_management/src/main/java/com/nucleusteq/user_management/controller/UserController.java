441e9524782ab5200e5332ad1380959ef449de50
manasvijain06 <manasvijain61104@gmail.com>
2026-04-11 12:39:02 +0530

    feat: Develop UserController with REST APIs (GET all users, GET user by ID, POST create user)

---
+++ manasvijain_java_training/session2/user_management/src/main/java/com/nucleusteq/user_management/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.nucleusteq.user_management.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nucleusteq.user_management.model.User;
+import com.nucleusteq.user_management.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService service;
+
+    // Constructor Injection
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    // GET /users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    // POST /users
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return service.createUser(user);
+    }
+
+    // GET /users/{id}
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return service.getUserById(id);
+    }
+}
