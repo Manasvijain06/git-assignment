0973a9cbb5e18a526064ad1a4b4f7a3a65a07266
manasvijain06 <manasvijain61104@gmail.com>
2026-04-11 13:17:26 +0530

    feat:Add NotificationService to trigger notifications using NotificationComponent

---
+++ manasvijain_java_training/session2/user_management/src/main/java/com/nucleusteq/user_management/service/NotificationService.java
@@ -0,0 +1,19 @@
+package com.nucleusteq.user_management.service;
+
+import org.springframework.stereotype.Service;
+
+import com.nucleusteq.user_management.component.NotificationComponent;
+
+@Service
+public class NotificationService {
+
+    private final NotificationComponent component;
+
+    public NotificationService(NotificationComponent component) {
+        this.component = component;
+    }
+
+    public String triggerNotification(String name) {
+        return component.sendNotification(name);
+    }
+}
