6e5076be4d2327b7d6288f544a3fb418530fb894
manasvijain06 <manasvijain61104@gmail.com>
2026-04-14 15:46:56 +0530

    feat: Initialize the spring boot project setup

---
+++ manasvijain_java_training/session3/SpringAndRest/src/main/java/com/nucleusteq1/SpringAndRest/SpringAndRestApplication.java
@@ -0,0 +1,13 @@
+package com.nucleusteq1.SpringAndRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndRestApplication.class, args);
+	}
+
+}
---
+++ manasvijain_java_training/session3/SpringAndRest/src/test/java/com/nucleusteq1/SpringAndRest/SpringAndRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucleusteq1.SpringAndRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
