394eed6862acde96e72bc9819837a23bdce0d0fb
manasvijain06 <manasvijain61104@gmail.com>
2026-04-16 21:58:08 +0530

    feat: Initialize the project setup with basic structure

---
+++ manasvijain_java_training/session4/SpringAdvance/src/test/java/com/Nucleusteq/SpringAdvance/SpringAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nucleusteq.SpringAdvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
