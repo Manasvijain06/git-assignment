e5d909ddd23cb5a11e9e87ddae3ae814f4167ca1
manasvijain06 <manasvijain61104@gmail.com>
2026-04-18 01:30:47 +0530

    feat: Refactor code for readability, null safety and improved exception handling

--- manasvijain_java_training/session4/SpringAdvance/src/main/java/com/Nucleusteq/SpringAdvance/controller/TodoController.java
+++ manasvijain_java_training/session4/SpringAdvance/src/main/java/com/Nucleusteq/SpringAdvance/controller/TodoController.java
@@ -1,12 +1,21 @@
 package com.Nucleusteq.SpringAdvance.controller;
 
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-import com.Nucleusteq.SpringAdvance.dto.*;
-import com.Nucleusteq.SpringAdvance.service.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Nucleusteq.SpringAdvance.dto.TodoRequestDTO;
+import com.Nucleusteq.SpringAdvance.dto.TodoResponseDTO;
+import com.Nucleusteq.SpringAdvance.service.TodoService;
 
 import jakarta.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/todos")
