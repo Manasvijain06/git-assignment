24a73b21b8bbbb591fe46d7ab789fe9103a4ee5f
manasvijain06 <manasvijain61104@gmail.com>
2026-04-20 13:59:43 +0530

    feat: Added SLF4J logging in controller layer

--- manasvijain_java_training/session4/SpringAdvance/src/main/java/com/Nucleusteq/SpringAdvance/controller/TodoController.java
+++ manasvijain_java_training/session4/SpringAdvance/src/main/java/com/Nucleusteq/SpringAdvance/controller/TodoController.java
@@ -2,6 +2,8 @@ package com.Nucleusteq.SpringAdvance.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
+    private static final Logger logger = LoggerFactory.getLogger(TodoController.class);
     private final TodoService todoservice;
 
     public TodoController(TodoService todoservice) {
@@ -29,28 +32,32 @@ public class TodoController {
 
     @GetMapping
     public List<TodoResponseDTO> getAll() {
+        logger.info("Fetching all todos");
         return todoservice.getAll();
     }
 
     @GetMapping("/{id}")
     public TodoResponseDTO getById(@PathVariable Long id) {
+        logger.info("Fetching todo with id: {}", id);
         return todoservice.getById(id);
     } 
 
     @PostMapping
     public TodoResponseDTO createTodo(@RequestBody @Valid TodoRequestDTO dto) {
+        logger.info("Creating new todo with title: {}", dto.getTitle());
         return todoservice.createTodo(dto);
     }
 
     @PutMapping("/{id}")
     public TodoResponseDTO update(
-        @PathVariable Long id,
-        @Valid @RequestBody TodoRequestDTO dto) {
-        return todoservice.update(id, dto);
+        @PathVariable Long id,@Valid @RequestBody TodoRequestDTO dto) {
+            logger.info("Updating todo with id: {}", id);
+            return todoservice.update(id, dto);
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable Long id) {
+        logger.info("Deleting todo with id: {}", id);
         todoservice.delete(id);
         return "Deleted successfully";
     }
